2451c9131c30582bdb19ddc995564cfe287f8d97
Dmitriy <dm.tsiganov@gmail.com>
2025-04-27 11:54:59 +0700

    migration

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    category = models.CharField()
+    purchase_price = models.FloatField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+def __str__(self):
+    return f'{self.name} {self.description}'
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
